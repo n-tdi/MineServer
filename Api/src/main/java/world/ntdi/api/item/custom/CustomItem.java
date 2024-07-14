d1882b085c0ac329f33f976a8036501b48b57baf
nikkasouza <yogikngyt@gmail.com>
2024-07-13 22:14:32 -0400

    new custom item api

--- Api/src/main/java/world/ntdi/api/item/custom/CustomItem.java
+++ Api/src/main/java/world/ntdi/api/item/custom/CustomItem.java
@@ -3,37 +3,84 @@ package world.ntdi.api.item.custom;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import world.ntdi.api.Api;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import world.ntdi.api.item.builders.ItemBuilder;
 
+@Getter
 public abstract class CustomItem implements Listener {
-    @Getter
-    private final String m_name;
-    @Getter
+    private final int m_id;
+    private final String m_title;
     private final String[] m_description;
-    @Getter
     private final Material m_material;
+    private final NamespacedKey m_namespacedKey;
 
-    public CustomItem(String p_name, String[] p_description, Material p_material) {
-        this.m_name = p_name;
-        this.m_description = p_description;
-        this.m_material = p_material;
-        Bukkit.getServer().getPluginManager().registerEvents(this, Api.getInstance());
+    protected CustomItem(int p_id, String p_title, String[] p_description, Material p_material) {
+        m_id = p_id;
+        m_title = p_title;
+        m_description = p_description;
+        m_material = p_material;
+        m_namespacedKey = new NamespacedKey("tazkaboom", m_id + m_title);
+
+        Bukkit.getServer().getPluginManager().registerEvents(this, Bukkit.getPluginManager().getPlugin("Api"));
+    }
+
+    public ItemStack getItemStack() {
+        final ItemBuilder item = new ItemBuilder().material(m_material).amount(1).lore(m_description).name(m_title);
+
+        final ItemStack itemStack = item.build();
+        ItemMeta meta = itemStack.getItemMeta();
+
+        meta.getPersistentDataContainer().set(m_namespacedKey, PersistentDataType.INTEGER, m_id);
+
+        itemStack.setItemMeta(meta);
+
+        return itemStack;
+    }
+
+    public boolean isCustomItem(final ItemStack p_itemStack) {
+        final ItemMeta meta = p_itemStack.getItemMeta();
+        final PersistentDataContainer container = meta.getPersistentDataContainer();
+
+        if (container.has(m_namespacedKey, PersistentDataType.INTEGER)) {
+            return container.get(m_namespacedKey, PersistentDataType.INTEGER) == m_id;
+        }
+        return false;
     }
 
     @EventHandler
-    public void onInteract(PlayerInteractEvent p_playerInteractEvent) {
-        if (p_playerInteractEvent.getAction() == Action.LEFT_CLICK_AIR || p_playerInteractEvent.getAction() == Action.LEFT_CLICK_BLOCK) {
-            onLeftClick(p_playerInteractEvent);
+    public void onInteract(final PlayerInteractEvent p_playerInteractEvent) {
+        final ItemStack itemStack = p_playerInteractEvent.getItem();
+        if (itemStack == null || itemStack.getType() != m_material) {
             return;
         }
-        onRightClick(p_playerInteractEvent);
+
+
+        if (!isCustomItem(itemStack)) {
+            return;
+        }
+
+        p_playerInteractEvent.setCancelled(true);
+
+        if (p_playerInteractEvent.getAction() == Action.LEFT_CLICK_AIR || p_playerInteractEvent.getAction() == Action.LEFT_CLICK_BLOCK) {
+            final Player player = p_playerInteractEvent.getPlayer();
+            onLeftClick(player, p_playerInteractEvent);
+        } else if (p_playerInteractEvent.getAction() == Action.RIGHT_CLICK_AIR || p_playerInteractEvent.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            final Player player = p_playerInteractEvent.getPlayer();
+            onRightClick(player, p_playerInteractEvent);
+        }
     }
 
-    public abstract void onLeftClick(PlayerInteractEvent e);
-    public abstract void onRightClick(PlayerInteractEvent e);
+    public abstract void onLeftClick(final Player p_player, final PlayerInteractEvent p_playerInteractEvent);
+    public abstract void onRightClick(final Player p_player, final PlayerInteractEvent p_playerInteractEvent);
+
 
 }
