6740798ce9c2e1919708f90be402110699f9131b
nikkasouza <yogikngyt@gmail.com>
2024-07-16 18:10:43 -0400

    event priotiys

--- Api/src/main/java/world/ntdi/api/pet/PetListener.java
+++ Api/src/main/java/world/ntdi/api/pet/PetListener.java
@@ -2,6 +2,7 @@ package world.ntdi.api.pet;
 
 import lombok.AllArgsConstructor;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -10,12 +11,12 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class PetListener implements Listener {
     private final PetService m_petService;
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOW)
     public void onJoin(PlayerJoinEvent p_playerJoinEvent) {
         m_petService.playerJoin(p_playerJoinEvent.getPlayer().getUniqueId());
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     public void onQuit(PlayerQuitEvent p_playerQuitEvent) {
         m_petService.playerLeave(p_playerQuitEvent.getPlayer().getUniqueId());
     }
--- Api/src/main/java/world/ntdi/api/playerwrapper/PlayerWrapperListener.java
+++ Api/src/main/java/world/ntdi/api/playerwrapper/PlayerWrapperListener.java
@@ -2,18 +2,19 @@ package world.ntdi.api.playerwrapper;
 
 import lombok.AllArgsConstructor;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 @AllArgsConstructor
 public class PlayerWrapperListener implements Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOW)
     public void onJoin(PlayerJoinEvent p_playerJoinEvent) {
         PlayerWrapper.playerJoin(p_playerJoinEvent.getPlayer().getUniqueId());
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     public void onQuit(PlayerQuitEvent p_playerQuitEvent) {
         PlayerWrapper.playerQuit(p_playerQuitEvent.getPlayer().getUniqueId());
     }
