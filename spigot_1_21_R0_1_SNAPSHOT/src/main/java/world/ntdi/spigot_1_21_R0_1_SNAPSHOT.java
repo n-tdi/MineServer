e4ff2746d2e3a4434493cc91eade11942c485927
nikkasouza <yogikngyt@gmail.com>
2024-07-18 09:14:19 -0400

    bye bye nms

--- spigot_1_21_R0_1_SNAPSHOT/src/main/java/world/ntdi/spigot_1_21_R0_1_SNAPSHOT.java
+++ spigot_1_21_R0_1_SNAPSHOT/src/main/java/world/ntdi/spigot_1_21_R0_1_SNAPSHOT.java
@@ -1,14 +1,10 @@
 package world.ntdi;
 
-import net.minecraft.server.level.ServerPlayer;
-import org.bukkit.craftbukkit.v1_21_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import world.ntdi.api.nms.ChangePlayerNameService;
 
 public class spigot_1_21_R0_1_SNAPSHOT implements ChangePlayerNameService {
     @Override
     public void updatePlayerUsername(String p_username, Player p_player) {
-        CraftPlayer craftPlayer = (CraftPlayer) p_player;
-        ServerPlayer serverPlayer = craftPlayer.getHandle();
     }
 }
